ea6fbecfb5a6bc3e3ce389d0eed6e440b5d8e36c
prvnsingh <prvns1997@gmail.com>
2017-01-31 00:44:29 +0530

    modified change.cpp

--- greedy algo/change.cpp
+++ greedy algo/change.cpp
@@ -1,17 +1,16 @@
 #include <iostream>
-
-int get_change(int m) {
+int get_change(int m)
+ {
   //write your code here
   int c = 0;
   while(m > 0)
   {
-  	if(m > 10)
+  	if(m >= 10)
   	{
   		c = c + m / 10;
   		m=m % 10;
   	}
-  	else
-	  if(m > 5)
+  	else if(m >= 5)
   	{
   		c = c + m / 5;
   		m = m % 5;
